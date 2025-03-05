d2d2dec41855107c2e1758ed85f69eabbb071dc8
Sleiman El Yahfoufi <36049149+SleiYah@users.noreply.github.com>
2025-03-06 01:27:39 +0200

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,5 @@
 
-const baseURL = "http://localhost/projects/SI-Wallet/Wallet-Server"
+const baseURL = "http://51.44.8.213/SI-Wallet/Wallet-Server"
 
 document.addEventListener('DOMContentLoaded', function() {
   
